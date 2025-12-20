c339cc0faca2bdea1af162918074b676049d4f9d
night <wachayathorn>
2025-12-20 11:15:30 +0700

    refactor MaxProduct function to correctly calculate and return the maximum product of a subarray

--- ploblem/maximum-product-subarray.go
+++ ploblem/maximum-product-subarray.go
@@ -1,11 +1,37 @@
 package ploblem
 
+import (
+	"fmt"
+)
+
 func MaxProduct(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
 
-	var result int
+	var (
+		sum        int
+		numsResult []int
+	)
 
-	return result
+	for i, num := range nums {
+		currentNumsResult := []int{num}
+		currentValue := num
+		for j := i + 1; j < len(nums); j++ {
+			currentNumsResult = append(currentNumsResult, nums[j])
+			currentValue = currentValue * nums[j]
+			if sum < currentValue {
+				numsResult = currentNumsResult
+				sum = currentValue
+			}
+			if sum > currentValue && j != i+1 {
+				break
+			}
+		}
+	}
+
+	fmt.Println(sum)
+	fmt.Println(numsResult)
+
+	return sum
 }
