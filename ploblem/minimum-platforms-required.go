f1a0dc4ceefed01a5060f3c09467345955f437d2
night <wachayathorn>
2025-12-27 12:05:59 +0700

    completed train

--- ploblem/minimum-platforms-required.go
+++ ploblem/minimum-platforms-required.go
@@ -1,27 +1,33 @@
 package ploblem
 
 func MinimumPlatformsRequired(arr, dep []int) int {
-	result := 0
-
 	trains := len(arr)
+
+	result := 1
 
 	for i := 0; i < trains; i++ {
 		currentTrainArrTime := arr[i]
 		currentTrainDepTime := dep[i]
 
+		platform := 1
+
 		for j := i + 1; j < trains; j++ {
 			nextTrainArrTime := arr[j]
 			nextTrainDepTime := dep[j]
 
-			if currentTrainArrTime >= nextTrainArrTime || currentTrainDepTime <= nextTrainDepTime {
-				result++
+			if currentTrainArrTime >= nextTrainArrTime && currentTrainDepTime <= nextTrainDepTime {
+				platform++
 				continue
 			}
 
-			if nextTrainArrTime >= currentTrainArrTime || nextTrainDepTime <= currentTrainDepTime {
-				result++
+			if nextTrainArrTime <= currentTrainDepTime {
+				platform++
 				continue
 			}
+		}
+
+		if result < platform {
+			result = platform
 		}
 	}
 
