246db717144b7340384b327e5868cdcd6a109d78
night <wachayathorn>
2025-12-27 13:25:51 +0700

    in place array

--- ploblem/array-rotations.go
+++ ploblem/array-rotations.go
@@ -1,12 +1,16 @@
 package ploblem
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ArrayRotations(arr []int, k int) {
 	for i := 1; i <= k; i++ {
-		lastValue := arr[len(arr)-1:][0]
-		arr = arr[0 : len(arr)-1]
-		arr = append([]int{lastValue}, arr...)
+		for j := len(arr) - 1; j > 0; j-- {
+			temp := arr[j]
+			arr[j] = arr[j-1]
+			arr[j-1] = temp
+		}
 	}
 	fmt.Println("Result : ", arr)
 }
