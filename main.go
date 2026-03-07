116afb9dd37a620e2a037b538e260cc568075c06
night <wachayathorn.s>
2026-03-07 21:57:59 +0700

    implement MinDeletions function to calculate minimum deletions for good strings

--- main.go
+++ main.go
@@ -2,10 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/wachayathorn/quiz-problems/ploblem"
 )
 
 func main() {
-	fmt.Println(ploblem.RemoveDuplicateLetters("bcabc"))
+	result := ploblem.MinDeletions("aab")
+	if result != 0 {
+		log.Fatalf("case 1 error got : %d", result)
+	} else {
+		fmt.Println("case 1 success")
+	}
+
+	result = ploblem.MinDeletions("aaabbbcc")
+	if result != 2 {
+		log.Fatalf("case 2 error got : %d", result)
+	} else {
+		fmt.Println("case 2 success")
+	}
+
+	result = ploblem.MinDeletions("ceabaacb")
+	if result != 2 {
+		log.Fatalf("case 3 error got : %d", result)
+	} else {
+		fmt.Println("case 3 success")
+	}
+
+	result = ploblem.MinDeletions("accdcdadddbaadbc")
+	if result != 1 {
+		log.Fatalf("case 4 error got : %d", result)
+	} else {
+		fmt.Println("case 4 success")
+	}
+
+	result = ploblem.MinDeletions("abcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwzabcdefghijklmnopqrstuvwxwz")
+	if result != 276 {
+		log.Fatalf("case 5 error got : %d", result)
+	} else {
+		fmt.Println("case 5 success")
+	}
+
+	result = ploblem.MinDeletions("bbcebab")
+	if result != 2 {
+		log.Fatalf("case 6 error got : %d", result)
+	} else {
+		fmt.Println("case 6 success")
+	}
+
 }
---
+++ ploblem/min-deletions.go
@@ -0,0 +1,55 @@
+package ploblem
+
+import (
+	"sort"
+)
+
+// A string s is called good if there are no two different characters in s that have the same frequency.
+// Given a string s, return the minimum number of characters you need to delete to make s good.
+// The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+func MinDeletions(s string) int {
+	strWithCount := make(map[string]int)
+	for _, c := range s {
+		_, ok := strWithCount[string(c)]
+		if !ok {
+			strWithCount[string(c)] = 1
+		} else {
+			strWithCount[string(c)]++
+		}
+	}
+
+	type KeyValuePair struct {
+		Key   string
+		Value int
+	}
+
+	var pairs []KeyValuePair
+	for k, v := range strWithCount {
+		pairs = append(pairs, KeyValuePair{k, v})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].Value < pairs[j].Value
+	})
+
+	uniqueValue := make(map[int]bool)
+
+	deleted := 0
+	for i := 0; i < len(pairs); i++ {
+		isGo := true
+		for isGo {
+			_, ok := uniqueValue[pairs[i].Value]
+			if !ok {
+				uniqueValue[pairs[i].Value] = true
+				isGo = false
+			} else if pairs[i].Value == 0 {
+				isGo = false
+			} else {
+				pairs[i].Value--
+				deleted++
+			}
+		}
+	}
+
+	return deleted
+}
